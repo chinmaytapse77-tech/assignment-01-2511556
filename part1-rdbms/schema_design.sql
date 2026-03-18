7c8ef5fcf5831c76f5fd259785ec2aeae84b9b9a
chinmaytapse77-tech <chinmaytapse77@gmail.com>
2026-03-18 11:47:57 -0400

    Update schema_design.sql

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -1,1 +1,39 @@
+Customers Table
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    city VARCHAR(100) NOT NULL
+);
 
+Products Table
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL
+);
+
+Sales Representatives Table
+CREATE TABLE sales_reps (
+    salesrep_id INT PRIMARY KEY,
+    salesrep_name VARCHAR(100) NOT NULL
+);
+
+Orders Table
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    salesrep_id INT NOT NULL,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (salesrep_id) REFERENCES sales_reps(salesrep_id)
+);
+
+Order Items Table
+CREATE TABLE order_items (
+    order_item_id INT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
